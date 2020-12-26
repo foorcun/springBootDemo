170141e66ddf1eef3da8d8d6b23cc7c6c45c52e4
User <User@192.168.1.3>
2020-12-26 12:07:27 +0200

    first commit

---
+++ springBootDemo/src/main/java/com/javacourse/springBootDemo/apiDemo/ProductsController.java
@@ -0,0 +1,29 @@
+package com.javacourse.springBootDemo.apiDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+
+	@GetMapping("/")
+	public String get() {
+		
+		return "Laptop";
+	}
+	
+	
+	@GetMapping("/products")
+	public String get2() {
+		
+		return "Laptop 2";
+	}
+	
+	@GetMapping("/lalala")
+	public String get3() {
+		
+		return "Laptop lalala";
+	}
+	
+	
+}
